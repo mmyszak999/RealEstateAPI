5f185f8fd7730e38e329f9821abbd2f4015edf57
michal999 <71217112+mmyszak999@users.noreply.github.com>
2024-08-26 20:49:28 +0200

    add company model and schemas

---
+++ src/apps/company/schemas.py
@@ -0,0 +1,50 @@
+import datetime
+from typing import Any, Optional
+
+from pydantic import BaseModel, EmailStr, Field, validator
+
+from src.apps.users.schemas import UserInfoOutputSchema
+
+
+class CompanyBaseSchema(BaseModel):
+    company_name: str = Field(max_length=100)
+    foundation_year: int = Field(ge=0)
+
+    class Config:
+        orm_mode = True
+
+
+class CompanyInputSchema(CompanyBaseSchema):
+    phone_number: str = Field(max_length=50)
+
+    class Config:
+        orm_mode = True
+
+
+class CompanyUpdateSchema(BaseModel):
+    company_name: Optional[str]
+    foundation_year: Optional[int] = Field(ge=0)
+    phone_number: Optional[str]
+
+
+    class Config:
+        orm_mode = True
+
+
+class CompanyInfoOutputSchema(CompanyBaseSchema):
+    id: str
+    
+
+    class Config:
+        orm_mode = True
+
+
+class CompanyOutputSchema(CompanyInfoOutputSchema):
+    users: Optional[list[UserInfoOutputSchema]]
+
+    class Config:
+        orm_mode = True
+
+
+class CompanyIdSchema(BaseModel):
+    id: str
