5cc7f9d3fbac7b60f0ad19cdd5b670a5585787da
michal999 <71217112+mmyszak999@users.noreply.github.com>
2024-08-30 14:16:13 +0200

    add lease factory

--- src/core/utils/time.py
+++ src/core/utils/time.py
@@ -8,3 +8,13 @@ def get_current_time():
 
 def set_employment_date_for_factory():
     return get_current_time() - datetime.timedelta(weeks=random.randint(4, 55))
+
+
+def generate_random_date(
+    start_date: datetime.date, end_date: datetime.date
+    ) -> datetime.date:
+
+    delta_days = (end_date - start_date).days
+    random_days = random.randint(0, delta_days)
+    
+    return start_date + datetime.timedelta(days=random_days)
